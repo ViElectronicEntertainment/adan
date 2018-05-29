d5d84bb8d296d91d90d0b0816f612566c151f919
Sergio Veloza <velozasergio@gmail.com>
2018-05-28 19:33:59 -0500

    0.0.0.2
    cambios al schema

--- app/database/schema.sql
+++ app/database/schema.sql
@@ -2,18 +2,17 @@ create database adan;
 use adan;
 
 create table usuarios ( 
-  id int not null auto_increment primary key,/*1*/
+  id int not null auto_increment primary key,
   /*Login Info*/
-  email varchar(255) not null,/*1*/
+  email varchar(255) not null,
   password varchar(60) not null,
   /*Blinter Profile*/
-  name varchar(20) not null, /*1*/
-  lastname varchar(20) not null,/*1*/
-  userpicture varchar(255) default 'default-avatar.jpg',/*1*/
-  level int(100) not null default 1,
+  name varchar(20) not null,
+  lastname varchar(20) not null,
+  userpicture varchar(255) default 'default-avatar.jpg',
   experience int(100) not null default 1,
   created_at datetime not null
   );
 
-insert into usuarios(email,password,name,lastname,userpicture,level,experience,leadership,innovation,teamwork,management,design,development,problemresolution,sales,communication,community,created_at)
-	value ("pami@ublinter.com","pami","PAMI","Adan","a.jpg",99,99,NOW());
+insert into usuarios(email,password,name,lastname,userpicture,experience,created_at)
+	value ("pami@ublinter.com","pami","PAMI","Adan","a.jpg",99,NOW());
